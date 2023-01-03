681ece682b5246eff1fba43990fef1026e52f47c
cywu <black46821379@gmail.com>
2023-01-03 19:00:42 +0800

    index.html 潤色

--- js/chunYi.js
+++ js/chunYi.js
@@ -3,15 +3,18 @@ const today=new Date();
 
 // 當下日期
 function getDatetime(){
-    date.innerHTML=(today.getMonth()+1) + " / " + today.getDate()
-}
+    date.innerHTML=(today.getMonth()+1) + " 月 " + today.getDate() + " 日 "
+};
 // 進入網頁執行
 getDatetime();
 
 // 切換
-const toggleButton=document.querySelector('#toggleButton')
-const body=document.querySelector('body')
+const toggleButton=document.querySelector('#toggleButton');
+const body=document.querySelector('body');
+const nav = document.querySelector('.nav');
 function toggle(){
     toggleButton.classList.toggle('light');
     body.classList.toggle('light');
+    nav.classList.toggle('light');
+    $(".background_image_1").toggle();
 }
